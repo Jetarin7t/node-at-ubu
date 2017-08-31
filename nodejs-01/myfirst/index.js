53dbf92fc392deafdd498fa0609eb6137eca4b00
Jetarin7t <jetarin.ta.57@ubu.ac.th>
2017-08-31 17:42:46 +0700

    add name

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color: green;">Hello World!</h1>');
+    res.write("<h1 style='color: red;'>Hello World!</h1>");
+    res.write("<h2 style='color: green;'>From</h2>");
+    res.write("<h1 style='color: blue;'>Jetarin Taloet 5711407085</h1>");
+    res.end();
 }).listen(8080);
